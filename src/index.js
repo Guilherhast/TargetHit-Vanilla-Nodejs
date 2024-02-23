3306f77b2107bfe1a4251cae4d824663e959a41a
Guilherme Hastenreiter Ferreira <hast.job@gmail.com>
2024-02-23 12:18:17 -0300

    Feat: Add api handler

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
-const http = require('node:http');
+const http = require('node:http'),
+	Url = require('node:url');
 
 // Docs
 // https://nodejs.org/api/http.html#httpcreateserveroptions-requestlistener
+// https://nodejs.org/api/url.html#url-strings-and-url-objects
 
-// Create a local server to receive data from
-const server = http.createServer((req, res) => {
+// Functions
+function isAPI(url){
+	const pathname = Url.parse(url).pathname.split('/');
+	return pathname[1].toLowerCase() == "api";
+}
+
+function handleAPI(req, res){
 	res.writeHead(200, {'Content-Type': 'application/json'});
-	
+	res.end(JSON.stringify({data: 'Hello from api'}));
+}
+
+function handleStatic(req, res){
+	res.writeHead(200, {'Content-Type': 'application/json'});
 	res.end(JSON.stringify({
 		data: 'Hello World!',
 	}));
+}
+
+// Create a local server to receive data from
+const server = http.createServer((req, res) => {
+
+	if (isAPI(req.url)) handleAPI(req, res);
+	else handleStatic(req, res);
+
 });
 
 server.listen(8000);
