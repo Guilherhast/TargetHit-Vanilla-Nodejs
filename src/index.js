0db328de7d03e00ad181c98a0cf40c67f63b0081
Guilherme Hastenreiter Ferreira <hast.job@gmail.com>
2024-02-23 11:55:22 -0300

    Feat: Added http server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Hi');
+const http = require('node:http');
+
+// Docs
+// https://nodejs.org/api/http.html#httpcreateserveroptions-requestlistener
+
+// Create a local server to receive data from
+const server = http.createServer((req, res) => {
+	res.writeHead(200, {'Content-Type': 'application/json'});
+	
+	res.end(JSON.stringify({
+		data: 'Hello World!',
+	}));
+});
+
+server.listen(8000);
