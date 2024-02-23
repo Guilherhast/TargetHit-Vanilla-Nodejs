5d47a1704fe8c6b55b591eed5acab92af9ec93c6
Guilherme Hastenreiter Ferreira <hast.job@gmail.com>
2024-02-23 11:33:05 -0300

    Initial commit

---
+++ src/public/network.js
@@ -0,0 +1,57 @@
+// Constants
+const scoresUrl = "./scores.php";
+
+
+// Variables
+let userName = "";
+
+// Functions
+// Local functions
+function login(){
+  const i = document.querySelector('#login input');
+  i.value = i.value.trim();
+  userName = i.value;
+}
+function onEnter(event) {
+  if(event.key == 'Enter') login();
+}
+
+// Network functions
+
+async function getScores(){
+  const resp = await fetch(scoresUrl, { cache: "no-cache" });
+  const ans = await resp.json();
+  return ans;
+}
+
+async function postScores(user, clicks, hits){
+  if ( user == '') return '';
+
+  const score = {
+    name: user,    
+    hits: hits,
+    clicks: clicks,
+  }
+  return await sendPostRequest(scoresUrl, score);
+}
+
+
+//
+function createFormData(data){
+  const str = JSON.stringify(data);
+  const fd = new FormData();
+  fd.append('data', str);
+  return fd;
+}
+
+async function sendPostRequest(url, obj) {
+  const headers = {
+    method: 'POST',
+    body: createFormData(obj)
+  }
+  
+  const req = await fetch(url, headers);
+
+
+  return await req.json();
+}
---
+++ src/public/script.js
@@ -0,0 +1,86 @@
+// Constants
+const roundTime = 8000;
+
+
+// Variables
+let nClicks = 0;
+let nHits = 0;
+
+// Functions
+// Game functions
+function rp() {
+    return Math.ceil(5 + Math.random() * 90) + '%';
+}
+function boardHit(){
+  ++nClicks;
+}
+function targetHit(){
+  ++nHits;
+  $('#target').attr('cx', rp());
+  $('#target').attr('cy', rp());
+}
+
+// Round functions
+async function posRound() {
+  $("#clicksDiv>.scValue").html(nClicks);
+  $("#hitsDiv>.scValue").html(nHits);
+  setTimeout(()=>$('#menu').css('pointer-events', 'all'), 800);
+  postScores(userName, nClicks, nHits);
+}
+
+function playRound(){
+  nClicks = 0;
+  nHits = 0;
+
+  $('#target').attr('cx', '50%');
+  $('#target').attr('cy', '50%');
+
+  
+  $('#menu').css('pointer-events', 'none')
+    .fadeOut(0).delay(roundTime).fadeIn(0);
+  $('#game').fadeIn(0).delay(roundTime).fadeOut(0, posRound);
+
+}
+
+//UI functions
+function showScreen(name) {
+  $('.screen').hide();
+  $('#'+name).show();
+}
+
+// Fill functions
+function makeScore(obj, index) {
+  return `
+<tr>
+    <td>${index+1}</td>
+    <td>${obj.name}</td>
+    <td>${obj.hits}</td>
+    <td>${obj.clicks}</td>
+</tr>
+  `;
+}
+function fillScores(list) {
+  $('#rankList').empty();
+  list.forEach( (el, i ) =>   
+    $('#rankList').append(makeScore(el,i))
+  );
+}
+
+// Liteners
+// Adding listeners
+$('#target').click(targetHit);
+$('#board').click(boardHit);
+
+$('#btnStart').click(playRound);
+
+// Navigation buttons
+$('#btnBack').click(()=>showScreen('menu'));
+$('#goBackRank').click(()=>showScreen('menu'));
+$('#btnLogin').click(()=>showScreen('login'));
+$('#btnRank').click(()=>{
+  getScores().then( all=>fillScores(all));
+  showScreen('ranking');
+});
+
+// Run code
+showScreen('menu');
